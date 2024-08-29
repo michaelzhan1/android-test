4ff45bafc97ed58ac904659f3c1baaa26cc8bedc
Michael Zhan <michaelzhan2001@gmail.com>
2024-08-28 23:18:48 -0500

    Add basic components

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.example.greetingcard
 
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore.Images.Media
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,19 +43,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             GreetingCardTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Actual Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column () {
 
-                    TestButton(
-                        onClick = {
-                            Log.i("Test", "Test")
-                        },
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                        Greeting(
+                            name = "Actual Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
+                        TestButton(
+                            onClick = {
+                                Log.i("Test", "Test")
+                            },
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
+                        ImageChooseAndDisplay()
+                    }
                 }
             }
+
         }
     }
 }
@@ -62,7 +86,64 @@ fun TestButton(onClick: () -> Unit, modifier: Modifier) {
     }
 }
 
-// TODO: make button open picture select, then display picture on choice
+@Composable
+fun ImageChooseAndDisplay() {
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var imageData by remember { mutableStateOf<Bitmap?>(null) }
+    val context = LocalContext.current
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri: Uri? ->
+            imageUri = uri
+            imageData = flipHorizontally(Media.getBitmap(context.contentResolver, uri))
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        Button(onClick = { imagePickerLauncher.launch("image/*") }) {
+            Text("Choose image")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        imageUri?.let {
+            Image(
+                painter = rememberAsyncImagePainter(it),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+        } ?: run {
+            Text(
+                text = "Choose an image"
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        imageData?.let {
+            AsyncImage(
+                model = imageData,
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+        } ?: run {
+            Text(
+                text = "Choose an image"
+            )
+        }
+    }
+}
+
+fun flipHorizontally(bitmap: Bitmap): Bitmap {
+    val matrix = Matrix().apply {
+        preScale(-1f, 1f)
+    }
+    return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, false)
+}
 
 @Preview(showBackground = true)
 @Composable
